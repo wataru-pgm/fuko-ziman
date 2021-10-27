8e38ec7be320ff18a0a87ad4610c53b1c9cb9898
Wataru Morikami <w.morikami.pgm@gmail.com>
2021-10-27 22:27:14 +0900

    add: 詳細画面のfontawsomeを変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,18 +1,4 @@
-console.log('good');
-
-
-// import Rails from "@rails/ujs"
-// import Turbolinks from "turbolinks"
-// import * as ActiveStorage from "@rails/activestorage"
-// import "channels"
-
 require("@rails/ujs").start()
-// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-// import '@fortawesome/fontawesome-free/js/all';
-require('../packs/hello')
 
-// Rails.start()
-// Turbolinks.start()
-// ActiveStorage.start()
