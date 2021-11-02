49453618bb1e7d2d473287e8d5488cd808b2e03e
Wataru Morikami <w.morikami.pgm@gmail.com>
2021-11-02 23:26:37 +0900

    add: エラーメッセージの表示にBulmaを適用させる

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,4 @@
 class Board < ApplicationRecord
   validates :title, {presence: true, length: {maximum: 10}}
-  validates :body, {presence: true, length: {maximum: 100}}
+  validates :body, {presence: true, length: {maximum: 200}}
 end
