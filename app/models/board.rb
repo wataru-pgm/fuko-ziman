213ffd86122049ae170f09ba397928bf04428750
Wataru Morikami <w.morikami.pgm@gmail.com>
2021-11-02 23:17:47 +0900

    fix: 内容の文字数制限を変更

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,4 @@
 class Board < ApplicationRecord
   validates :title, {presence: true, length: {maximum: 10}}
-  validates :body, {presence: true, length: {maximum: 200}}
+  validates :body, {presence: true, length: {maximum: 100}}
 end
