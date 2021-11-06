ad875dd6266d66a9758db3e3f5b06775bd90a894
Wataru Morikami <w.morikami.pgm@gmail.com>
2021-11-07 08:50:17 +0900

    add: ymlファイル作成

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,7 @@ module FukoZiman
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.generators.system_tests = nil
 
     config.generators do |g|
